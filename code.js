37d0e1f9eef8d4fa7863773617c40cd2e0ee76de
kommunna <120783632+kommunna@users.noreply.github.com>
2023-04-17 13:10:24 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector(".btn__yelow")
+let btn2 = document.getElementsByClassName("btn__head")
+console.log(btn, btn2)
+
+btn.addEventListener("click", function() {
+   this.style.backgroundColor = "red"
+})
+
+
+
+
+
